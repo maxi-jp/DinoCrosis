1ab3da24f443681d6b6eb6dcf0e77cb677a720b8
Maxi <juventudperdia@gmail.com>
2018-03-27 19:54:31 +0200

    Added a TRex model (CC https://sketchfab.com/models/516dbe9717554e74b0bfcbb77a431b17) with a very simple script for making it move

---
+++ Assets/Scripts/DinoController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DinoController : MonoBehaviour
+{
+
+    [HideInInspector]
+    public Animator animator;
+
+    [HideInInspector]
+    public NavMeshAgent agent;
+
+    private void Reset()
+    {
+        animator = transform.Find("Model").GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    void Start ()
+    {
+        agent.SetDestination(new Vector3(10f, 0f, 10f));
+
+    }
+	
+	void Update ()
+    {
+        animator.SetFloat("speed", agent.speed);
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    Debug.Log("Selecting new destination...");
+                    agent.SetDestination(new Vector3(Random.Range(-10.0f, 10.0f), 0f, Random.Range(-10.0f, 10.0f)));
+
+                }
+            }
+        }
+
+    }
+
+}
